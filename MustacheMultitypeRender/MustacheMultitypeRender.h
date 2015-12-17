4eccd7d062ff0a53734095279b6a198440d0e71d
Kenji Tayama <kenji.tayama@gmail.com>
2015-12-17 20:09:59 +0900

    Initial Commit

---
+++ MustacheMultitypeRender/MustacheMultitypeRender.h
@@ -0,0 +1,19 @@
+//
+//  MustacheMultitypeRender.h
+//  MustacheMultitypeRender
+//
+//  Created by Kenji Tayama on 12/17/15.
+//  Copyright © 2015 Kenji Tayama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MustacheMultitypeRender.
+FOUNDATION_EXPORT double MustacheMultitypeRenderVersionNumber;
+
+//! Project version string for MustacheMultitypeRender.
+FOUNDATION_EXPORT const unsigned char MustacheMultitypeRenderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MustacheMultitypeRender/PublicHeader.h>
+
+
